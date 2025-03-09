a1eae9549f45d3d034ce1a465c020ba5b6617b9c
Rochelle Awuor <awuorochelle75@gmail.com>
2025-03-09 14:25:36 +0300

    updates index.js

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 // Write your solution in this file!
-const employee={
-    name:"Chris",
-    streetAddress:"12 Kigali"
-}
+const employee ={
+    name:'Sam',
+    streetAddress:'11 Broadway'
 
+}
 function updateEmployeeWithKeyAndValue(employee,key,value){
-    
-    return { ...employee, [key]: value}
+    return {...employee,[key]:value}
 }
 function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
-    employee[key] = value
-    return employee
+    employee[key]=value;
+    return employee;
 }
 function deleteFromEmployeeByKey(employee,key){
-    const newEmployee={ ...employee}
-    delete newEmployee[key]
-    return newEmployee;
+    const newEmployee={...employee}
+    delete newEmployee[key];
+    return newEmployee
+    //delete newObj
 }
-function destructivelyDeleteFromEmployeeByKey(employe,key){
-    delete employee[key]
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+   delete employee[key];
     return employee
 }
